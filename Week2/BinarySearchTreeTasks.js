5810fe2d18306458a8f1fbde2d633c68dcf4d992
usmanrahim <usmanrahim2000@gmail.com>
2023-06-09 18:58:33 +0500

    binary trees

---
+++ Week2/BinarySearchTreeTasks.js
@@ -0,0 +1,13 @@
+// Complete the constructor function on the node. Store the data inside a data property on the instance.
+
+// Store null in properties left and right.
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+module.exports = Node;
